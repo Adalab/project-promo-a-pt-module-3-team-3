01b79893d7691d62c89ee4e3d19e829db3797515
Sara-Gavi <sara.gavilan.castro@gmail.com>
2024-01-27 13:20:38 +0100

    card preview component done

---
+++ src/components/Header.jsx
@@ -0,0 +1,26 @@
+import laptop from "../images/laptop-code-solid.svg";
+import logo from "../images/adalab.png";
+
+function Header() {
+  return (
+    <>
+      <header className="header">
+        <a
+          className="header__brand"
+          href="./"
+          title="Haz click para volver a la página inicial"
+        >
+          <img
+            className="header__companyLogo"
+            src={laptop}
+            alt="Logo proyectos molones"
+          />
+          <h1 className="header__title">Proyectos molones</h1>
+        </a>
+        <img className="logoSponsor" src={logo} alt="Logo Adalab" />
+      </header>
+    </>
+  );
+}
+
+export default Header;
