2147d9095eda0426e7f04f0042ffd9a8f155c6ef
Sara-Gavi <sara.gavilan.castro@gmail.com>
2024-01-27 13:30:20 +0100

    complete components and joined react of super Virginia

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,10 @@
 import "../scss/App.scss";
 import Header from "./Header.jsx";
-import logo from "../images/adalab.png";
-import { useState } from "react";
 import IndexSection from "./IndexSection.jsx";
 import CardPreview from "./CardPreview.jsx";
 import Footer from "./Footer.jsx";
+import Form from "./Form.jsx";
+import { useState } from "react";
 
 function App() {
   //  Parte Vir
@@ -30,101 +30,10 @@ function App() {
         <CardPreview job={job} author={author} />
 
         <form className="addForm">
-          <h2 className="title">Información</h2>
-          <fieldset className="addForm__group">
-            <legend className="addForm__title">
-              Cuéntanos sobre el proyecto
-            </legend>
-            <input
-              className="addForm__input"
-              type="text"
-              name="name"
-              id="name"
-              placeholder="Nombre del proyecto"
-            />
-            <input
-              className="addForm__input"
-              type="text"
-              name="slogan"
-              id="slogan"
-              placeholder="Slogan"
-            />
-            <div className="addForm__2col">
-              <input
-                className="addForm__input"
-                type="url"
-                name="repo"
-                id="repo"
-                placeholder="Repositorio"
-              />
-              <input
-                className="addForm__input"
-                type="url"
-                name="demo"
-                id="demo"
-                placeholder="Demo"
-              />
-            </div>
-            <input
-              className="addForm__input"
-              type="text"
-              name="technologies"
-              id="technologies"
-              placeholder="Tecnologías"
-            />
-            <textarea
-              className="addForm__input"
-              type="text"
-              name="desc"
-              id="desc"
-              placeholder="Descripción"
-              rows="5"
-            ></textarea>
-          </fieldset>
-
-          <fieldset className="addForm__group">
-            <legend className="addForm__title">
-              Cuéntanos sobre la autora
-            </legend>
-            <input
-              className="addForm__input"
-              type="text"
-              name="autor"
-              id="autor"
-              placeholder="Nombre"
-              onInput={handleClickAuthorName}
-            />
-            <input
-              className="addForm__input"
-              type="text"
-              name="job"
-              id="job"
-              placeholder="Trabajo"
-              onInput={handleClickJob}
-            />
-          </fieldset>
-
-          <fieldset className="addForm__group--upload">
-            <label htmlFor="image" className="button">
-              Subir foto del proyecto
-            </label>
-            <input
-              className="addForm__hidden"
-              type="file"
-              name="image"
-              id="image"
-            />
-            <label htmlFor="photo" className="button">
-              Subir foto de la autora
-            </label>
-            <input
-              className="addForm__hidden"
-              type="file"
-              name="photo"
-              id="photo"
-            />
-            <button className="button--large">Guardar proyecto</button>
-          </fieldset>
+          <Form
+            handleClickAuthorName={handleClickAuthorName}
+            handleClickJob={handleClickJob}
+          />
         </form>
       </main>
       <Footer />
---
+++ src/components/Form.jsx
@@ -0,0 +1,99 @@
+function Form(props) {
+  return (
+    <>
+      <h2 className="title">Información</h2>
+      <fieldset className="addForm__group">
+        <legend className="addForm__title">Cuéntanos sobre el proyecto</legend>
+        <input
+          className="addForm__input"
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Nombre del proyecto"
+        />
+        <input
+          className="addForm__input"
+          type="text"
+          name="slogan"
+          id="slogan"
+          placeholder="Slogan"
+        />
+        <div className="addForm__2col">
+          <input
+            className="addForm__input"
+            type="url"
+            name="repo"
+            id="repo"
+            placeholder="Repositorio"
+          />
+          <input
+            className="addForm__input"
+            type="url"
+            name="demo"
+            id="demo"
+            placeholder="Demo"
+          />
+        </div>
+        <input
+          className="addForm__input"
+          type="text"
+          name="technologies"
+          id="technologies"
+          placeholder="Tecnologías"
+        />
+        <textarea
+          className="addForm__input"
+          type="text"
+          name="desc"
+          id="desc"
+          placeholder="Descripción"
+          rows="5"
+        ></textarea>
+      </fieldset>
+
+      <fieldset className="addForm__group">
+        <legend className="addForm__title">Cuéntanos sobre la autora</legend>
+        <input
+          className="addForm__input"
+          type="text"
+          name="autor"
+          id="autor"
+          placeholder="Nombre"
+          onInput={props.handleClickAuthorName}
+        />
+        <input
+          className="addForm__input"
+          type="text"
+          name="job"
+          id="job"
+          placeholder="Trabajo"
+          onInput={props.handleClickJob}
+        />
+      </fieldset>
+
+      <fieldset className="addForm__group--upload">
+        <label htmlFor="image" className="button">
+          Subir foto del proyecto
+        </label>
+        <input
+          className="addForm__hidden"
+          type="file"
+          name="image"
+          id="image"
+        />
+        <label htmlFor="photo" className="button">
+          Subir foto de la autora
+        </label>
+        <input
+          className="addForm__hidden"
+          type="file"
+          name="photo"
+          id="photo"
+        />
+        <button className="button--large">Guardar proyecto</button>
+      </fieldset>
+    </>
+  );
+}
+
+export default Form;
