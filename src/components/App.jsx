5ed7a1986c700e64c5686ec9d9249ad73a972e84
Sara-Gavi <sara.gavilan.castro@gmail.com>
2024-02-10 15:24:42 +0100

    router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,10 +5,10 @@ import CardPreview from "./CardPreview.jsx";
 import Footer from "./Footer.jsx";
 import Form from "./Form.jsx";
 import { useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
+import Landing from "./Landing.jsx";
 
 function App() {
-
-
   const [data, setData] = useState({
     name: "",
     slogan: "",
@@ -23,30 +23,27 @@ function App() {
   });
 
   const updateData = (fieldName, userValue) => {
-    setData({...data,[ fieldName]: userValue})
-  }
-// Falta cambiarlo en el cardPreview para que se cambie en la tarjeta////
-  
+    setData({ ...data, [fieldName]: userValue });
+  };
+  // Falta cambiarlo en el cardPreview para que se cambie en la tarjeta////
+
   const updateAvatar = (fieldName, image) => {
-    setData({...data,[ fieldName]: image})
-
-  }
-  
-
+    setData({ ...data, [fieldName]: image });
+  };
 
   return (
     <div className="container">
       <Header />
 
       <main className="main">
+        <Routes>
+          <Route path="/" element={<Landing/>}></Route>
+        </Routes>
         <IndexSection />
-        <CardPreview data={data}/>
+        <CardPreview data={data} />
 
         <form className="addForm">
-          <Form
-            updateData={updateData}
-            updateAvatar={updateAvatar}
-          />
+          <Form updateData={updateData} updateAvatar={updateAvatar} />
         </form>
       </main>
       <Footer />
---
+++ src/components/Landing.jsx
@@ -0,0 +1,15 @@
+function Landing() {
+  return (
+    <section class="hero__content">
+      <h1 class="content__title">Natura Puzzle</h1>
+      <p class="content__slogan">Construye un Mundo Sostenible Pieza a Pieza</p>
+      <div class="content__button">
+        <a class="button__a" href="#">
+          ¡A Jugar!
+        </a>
+      </div>
+    </section>
+  );
+}
+
+export default Landing;
